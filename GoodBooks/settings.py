a1779ebef93ed754288a0956e986889072248230
nishavak <nishavak.n@somaiya.edu>
2020-05-03 12:13:46 +0530

    project for submission

--- GoodBooks/settings.py
+++ GoodBooks/settings.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import os
 import json
-with open('config.json') as config_file:
-    config = json.load(config_file)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
 LOGIN_REDIRECT_URL = ''
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
